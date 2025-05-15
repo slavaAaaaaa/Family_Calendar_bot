0ba7e5d8054ba3d5099f641ef5d88286fc9fb808
slavaAaaaaa <158818301+slavaAaaaaa@users.noreply.github.com>
2025-05-15 23:14:47 +0300

    Update comfirm_notif.py

--- Family_Calendar_bot/Family_Calendar/keyboards/comfirm_notif.py
+++ Family_Calendar_bot/Family_Calendar/keyboards/comfirm_notif.py
@@ -3,5 +3,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 notif_kb = InlineKeyboardMarkup(
     inline_keyboard=[[InlineKeyboardButton(text="Создать уведомление", callback_data="create_notif")],
                      [InlineKeyboardButton(text="Не создавать уведомление", callback_data='cancel_notif')],
+                     [InlineKeyboardButton(text="Отправить пользователю", callback_data="send_event")]
                      ]
 )
